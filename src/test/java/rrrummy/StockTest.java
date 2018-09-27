84ab274f55f8fc891528e25f66f4b71cc5b5244c
選ばれた観測者 <dishysun@hotmail.com>
2018-09-27 16:53:36 -0400

    added test cases for Stock class
    + StockTest.java

---
+++ src/test/java/rrrummy/StockTest.java
@@ -0,0 +1,29 @@
+package rrrummy;
+import junit.framework.TestCase;
+
+public class StockTest extends TestCase{
+	private Stock testStock1;
+	private Stock testStock2;
+	
+	public void testConstructor() {
+		testStock1 = new Stock();
+		testStock2 = new Stock();
+		//total tiles = 52 * 2 + 2
+		assertEquals(106, testStock1.size());
+		assertEquals(106, testStock2.size());
+	}
+	
+	public void testShuffle() {
+		testStock1 = new Stock();
+		testStock2 = new Stock();
+		Tile tile1;
+		Tile tile2;
+		int i = 0;
+		while  (testStock1.size() > 0 ) {
+			tile1 = testStock1.draw();
+			tile2 = testStock2.draw();
+			if (tile1.getColor() == tile2.getColor() && tile1.getNumber() == tile2.getNumber()) i++;
+		}
+		assertTrue(i < 20);
+	}
+}
