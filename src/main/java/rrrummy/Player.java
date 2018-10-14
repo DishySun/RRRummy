9f3d7aff1918dabe8e4ccca3cab259aec944390a
Yuanming Xiao <china.hunterxiao@gmail.com>
2018-10-14 15:54:19 -0400

    + Player.java
    added Player class

---
+++ src/main/java/rrrummy/Player.java
@@ -0,0 +1,35 @@
+package rrrummy;
+
+public class Player {
+	private Hand hand;
+	private int score;
+	private String name;
+	public Player(String n) {
+		name = n;
+		hand = new Hand();
+		score = 0;
+	}
+	public void draw(Tile tile) {
+		// TODO Auto-generated method stub
+		
+	}
+	public Hand getHandCard() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public int handSize() {
+		return 0;
+		// TODO Auto-generated method stub
+	}
+	public Tile getHand(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public Tile play(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+	
+}
