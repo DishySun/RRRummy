7060e8298dc9a51da52996979bfc4be61c5dbabe
De Tao <td950614@gmail.com>
2018-10-14 15:40:23 -0400

    *MeldTest.java class
    change test method addHead and addTail for non-joker cards to add

--- src/test/java/rrrummy/MeldTest.java
+++ src/test/java/rrrummy/MeldTest.java
@@ -1,9 +1,10 @@
 package rrrummy;
+
 import java.util.ArrayList;
 
 import junit.framework.TestCase;
 
-public class MeldTest extends TestCase{
+public class MeldTest extends TestCase {
 	private Meld testMeld;
 	private Tile joker;
 	private Tile r1;
@@ -17,6 +18,7 @@ public class MeldTest extends TestCase{
 	private Tile b8;
 	private Tile o8;
 	private Tile g8;
+
 	private void init() {
 		joker = new Tile();
 		try {
@@ -31,229 +33,282 @@ public class MeldTest extends TestCase{
 			b8 = new Tile("B8");
 			o8 = new Tile("O8");
 			g8 = new Tile("G8");
-		}catch (InvalidTileException e) {
+		} catch (InvalidTileException e) {
 			fail(e.getErrMsg());
 		}
 		testMeld = new Meld();
 	}
+
 	public void testConstructor() {
 		init();
 		assertEquals(0, testMeld.size());
 	}
-	
+
 	public void testAdd() {
-		//add as a run
+		// add as a run
 		init();
-		assertEquals(0, testMeld.size());
-		assertTrue(testMeld.addHead(r4));
-		assertEquals(1, testMeld.size());
-		assertFalse(testMeld.addHead(g8));
-		assertEquals(1, testMeld.size());
-		assertTrue(testMeld.addTail(r5));
-		assertTrue(testMeld.addTail(r6));
-		assertFalse(testMeld.addTail(r2));
-		assertTrue(testMeld.addHead(r3));
-		assertTrue(testMeld.addHead(r2));
-		assertTrue(testMeld.addHead(r1));
-		assertEquals(6, testMeld.size());
+		try {
+			assertEquals(0, testMeld.size());
+			assertTrue(testMeld.add(r4));
+			assertEquals(1, testMeld.size());
+			assertFalse(testMeld.add(g8));
+			assertEquals(1, testMeld.size());
+			assertTrue(testMeld.add(r5));
+			assertTrue(testMeld.add(r6));
+			assertFalse(testMeld.add(r2));
+			assertTrue(testMeld.add(r3));
+			assertTrue(testMeld.add(r2));
+			assertTrue(testMeld.add(r1));
+			assertEquals(6, testMeld.size());
+		} catch (AbleToAddBothSideException e) {
+			fail();
+		}
 	}
-	
+
 	public void testAdd2() {
-		//add as a set
+		// add as a set
 		init();
-		assertEquals(0, testMeld.size());
-		assertTrue(testMeld.addHead(r8));
-		assertEquals(1, testMeld.size());
-		assertTrue(testMeld.addHead(g8));
-		assertEquals(2, testMeld.size());
-		assertTrue(testMeld.addTail(o8));
-		assertTrue(testMeld.addTail(b8));
-		//set can only have up to 4 tiles
-		assertFalse(testMeld.addHead(b8));
-		assertFalse(testMeld.addHead(joker));
+		try {
+			assertEquals(0, testMeld.size());
+			assertTrue(testMeld.add(r8));
+			assertEquals(1, testMeld.size());
+			assertTrue(testMeld.add(g8));
+			assertEquals(2, testMeld.size());
+			assertTrue(testMeld.add(o8));
+			assertTrue(testMeld.add(b8));
+			// set can only have up to 4 tiles
+			assertFalse(testMeld.add(b8));
+			assertFalse(testMeld.addHead(joker));
+		} catch (AbleToAddBothSideException e) {
+			fail();
+		}
 	}
-	
+
 	public void testAddJoker() {
 		init();
-		//joker can be added because joker is the first tile in this meld
+		// joker can be added because joker is the first tile in this meld
 		try {
 			assertTrue(testMeld.add(joker));
-		}catch(AbleToAddBothSideException e) {
+
+			assertTrue(testMeld.add(g8));
+			assertFalse(testMeld.add(r7));
+			assertTrue(testMeld.add(b8));
+			// after above, Tile can only be added as set from now
+			assertTrue(testMeld.add(o8));
+			assertEquals(4, testMeld.size());
+		} catch (AbleToAddBothSideException e) {
 			fail("Joker can be added by using add() method to an empty meld");
 		}
-		assertTrue(testMeld.addTail(g8));
-		assertFalse(testMeld.addHead(r7));
-		assertTrue(testMeld.addTail(b8));
-		//after above, Tile can only be added as set from now
-		assertTrue(testMeld.addTail(o8));
-		assertEquals(4, testMeld.size());
 	}
-	
+
 	public void testAddJoker2() {
 		init();
-		assertTrue(testMeld.addHead(r2));
 		try {
-			testMeld.add(joker); 
+			assertTrue(testMeld.add(r2));
+		}catch(AbleToAddBothSideException e) {
+			fail();
+		}
+		try {
+			testMeld.add(joker);
 			fail("add() method CANNOT be used to add a joker when both side can be added");
-		}catch (AbleToAddBothSideException e) {}
-		assertTrue(testMeld.addHead(r1));
+		} catch (AbleToAddBothSideException e) {
+		}
+		try {
+		assertTrue(testMeld.add(r1));
+		}
+		catch(AbleToAddBothSideException e) {
+			fail();
+		}
 		try {
 			assertTrue(testMeld.add(joker));
-		}catch (AbleToAddBothSideException e) {
+		} catch (AbleToAddBothSideException e) {
 			fail("Joker can be added by using add() method when only one side is able to add to");
 		}
 	}
+
 	public void testAddJoker3() {
 		init();
-		assertTrue(testMeld.addHead(r2));
-		assertTrue(testMeld.addTail(r3));
-		assertTrue(testMeld.addHead(joker)); 
-		assertTrue(testMeld.addTail(joker));
+		try {
+			assertTrue(testMeld.add(r2));
+			assertTrue(testMeld.add(r3));
+			assertTrue(testMeld.addHead(joker));
+			assertTrue(testMeld.addTail(joker));
+		} catch (AbleToAddBothSideException e) {
+			fail();
+		}
 	}
-	
+
 	public void testAddJoker4() {
 		init();
-		assertTrue(testMeld.addHead(joker)); 
-		assertTrue(testMeld.addTail(joker));
-		assertTrue(testMeld.addTail(r2));
+		try {
+			assertTrue(testMeld.addHead(joker));
+			assertTrue(testMeld.addTail(joker));
+			assertTrue(testMeld.add(r2));
+		} catch (AbleToAddBothSideException e) {
+			fail();
+		}
 	}
-	
+
 	public void testRemove() {
 		init();
-		assertTrue(testMeld.addTail(r4));
-		assertTrue(testMeld.addTail(r5));
-		assertTrue(testMeld.addTail(r6));
-		assertTrue(testMeld.addTail(r7));
-		assertEquals(4, testMeld.size());
-		//test removeHead
-		Tile t = testMeld.removeHead();
-		assertEquals(t, r4);
-		assertEquals(3, testMeld.size());
-		//test removeTail
-		Tile t2 = testMeld.removeTail();
-		assertEquals(t2, r7);
-		assertEquals(2,testMeld.size());
+		try {
+			assertTrue(testMeld.add(r4));
+			assertTrue(testMeld.add(r5));
+			assertTrue(testMeld.add(r6));
+			assertTrue(testMeld.add(r7));
+			assertEquals(4, testMeld.size());
+			// test removeHead
+			Tile t = testMeld.removeHead();
+			assertEquals(t, r4);
+			assertEquals(3, testMeld.size());
+			// test removeTail
+			Tile t2 = testMeld.removeTail();
+			assertEquals(t2, r7);
+			assertEquals(2, testMeld.size());
+		} catch (AbleToAddBothSideException e) {
+			fail();
+		}
 	}
-	
+
 	public void testCut() {
 		init();
-		assertTrue(testMeld.addHead(r1));
-		assertTrue(testMeld.addTail(r2));
-		assertTrue(testMeld.addTail(r3));
-		assertTrue(testMeld.addTail(r4));
-		assertTrue(testMeld.addTail(r5));
-		assertTrue(testMeld.addTail(r6));
-		assertTrue(testMeld.addTail(r7));
-		assertTrue(testMeld.addTail(r8));
-		ArrayList<Tile> returnArr = testMeld.cut(3); // cut at r4
-		assertEquals(4, testMeld.size());
-		assertEquals(4, returnArr.size());
-		assertEquals(Tile.Color.RED, returnArr.get(0).getColor());
-		assertEquals(1, returnArr.get(0).getNumber());
-		assertEquals(Tile.Color.RED, returnArr.get(returnArr.size()-1).getColor());
-		assertEquals(4, returnArr.get(returnArr.size()-1).getNumber());
-		assertEquals(r5, testMeld.removeHead());
-		assertEquals(r8, testMeld.removeTail());
+		try {
+			assertTrue(testMeld.add(r1));
+			assertTrue(testMeld.add(r2));
+			assertTrue(testMeld.add(r3));
+			assertTrue(testMeld.add(r4));
+			assertTrue(testMeld.add(r5));
+			assertTrue(testMeld.add(r6));
+			assertTrue(testMeld.add(r7));
+			assertTrue(testMeld.add(r8));
+			ArrayList<Tile> returnArr = testMeld.cut(3); // cut at r4
+			assertEquals(4, testMeld.size());
+			assertEquals(4, returnArr.size());
+			assertEquals(Tile.Color.RED, returnArr.get(0).getColor());
+			assertEquals(1, returnArr.get(0).getNumber());
+			assertEquals(Tile.Color.RED, returnArr.get(returnArr.size() - 1).getColor());
+			assertEquals(4, returnArr.get(returnArr.size() - 1).getNumber());
+			assertEquals(r5, testMeld.removeHead());
+			assertEquals(r8, testMeld.removeTail());
+		} catch (AbleToAddBothSideException e) {
+			fail();
+		}
 	}
-	
+
 	public void testReplace() {
 		init();
-		assertTrue(testMeld.addTail(r1));
-		assertTrue(testMeld.addTail(r2));
-		assertTrue(testMeld.addTail(r3));
-		assertTrue(testMeld.addTail(r4));
-		assertTrue(testMeld.addTail(joker));
-		assertTrue(testMeld.addTail(r6));
-		assertTrue(testMeld.addTail(r7));
-		assertTrue(testMeld.addTail(r8));
-		assertEquals(null, testMeld.replace(g8));
-		assertEquals(joker, testMeld.replace(r5));
+		try {
+			assertTrue(testMeld.add(r1));
+			assertTrue(testMeld.add(r2));
+			assertTrue(testMeld.add(r3));
+			assertTrue(testMeld.add(r4));
+			assertTrue(testMeld.addTail(joker));
+			assertTrue(testMeld.add(r6));
+			assertTrue(testMeld.add(r7));
+			assertTrue(testMeld.add(r8));
+			assertEquals(null, testMeld.replace(g8));
+			assertEquals(joker, testMeld.replace(r5));
+		} catch (AbleToAddBothSideException e) {
+			fail();
+		}
 	}
-	
+
 	public void tsetIsValid() {
 		init();
-		assertTrue(testMeld.addTail(r1));
-		assertFalse(testMeld.isValid());
-		assertTrue(testMeld.addTail(r2));
-		assertFalse(testMeld.isValid());
-		assertTrue(testMeld.addTail(joker));
-		assertTrue(testMeld.isValid());
-		assertTrue(testMeld.addTail(r4));
-		assertTrue(testMeld.isValid());
+		try {
+			assertTrue(testMeld.add(r1));
+			assertFalse(testMeld.isValid());
+			assertTrue(testMeld.add(r2));
+			assertFalse(testMeld.isValid());
+			assertTrue(testMeld.addTail(joker));
+			assertTrue(testMeld.isValid());
+			assertTrue(testMeld.add(r4));
+			assertTrue(testMeld.isValid());
+		} catch (AbleToAddBothSideException e) {
+			fail();
+		}
 	}
-	
+
 	public void testIsRunAndIsSet() {
 		init();
-		assertTrue(testMeld.isRun());
-		assertTrue(testMeld.isSet());
-		assertTrue(testMeld.addHead(r8));// R8
-		assertTrue(testMeld.isRun());
-		assertTrue(testMeld.isSet());
-		assertTrue(testMeld.addHead(g8));// G8 R8
-		assertFalse(testMeld.isRun());
-		assertTrue(testMeld.isSet());
-		assertTrue(testMeld.addTail(o8));// G8 R8 O8
-		assertFalse(testMeld.isRun());
-		assertTrue(testMeld.isSet());
-		assertEquals(o8, testMeld.removeTail());//G8 R8
-		assertFalse(testMeld.isRun());
-		assertTrue(testMeld.isSet());
-		assertEquals(g8, testMeld.removeHead()); //R8
-		assertTrue(testMeld.isRun());
-		assertTrue(testMeld.isSet());
-		assertTrue(testMeld.addHead(r7)); // R7 R8
-		assertTrue(testMeld.isRun());
-		assertFalse(testMeld.isSet());
-		assertTrue(testMeld.addHead(r6)); // R6 R7 R8
-		assertTrue(testMeld.isRun());
-		assertFalse(testMeld.isSet());
-		assertEquals(r6, testMeld.removeHead()); //R7 R8
-		assertTrue(testMeld.isRun());
-		assertFalse(testMeld.isSet());
-		assertEquals(r7, testMeld.removeHead()); // R8
-		assertTrue(testMeld.isRun());
-		assertTrue(testMeld.isSet());
+		try {
+			assertTrue(testMeld.isRun());
+			assertTrue(testMeld.isSet());
+			assertTrue(testMeld.add(r8));// R8
+			assertTrue(testMeld.isRun());
+			assertTrue(testMeld.isSet());
+			assertTrue(testMeld.add(g8));// G8 R8
+			assertFalse(testMeld.isRun());
+			assertTrue(testMeld.isSet());
+			assertTrue(testMeld.add(o8));// G8 R8 O8
+			assertFalse(testMeld.isRun());
+			assertTrue(testMeld.isSet());
+			assertEquals(o8, testMeld.removeTail());// G8 R8
+			assertFalse(testMeld.isRun());
+			assertTrue(testMeld.isSet());
+			assertEquals(g8, testMeld.removeHead()); // R8
+			assertTrue(testMeld.isRun());
+			assertTrue(testMeld.isSet());
+			assertTrue(testMeld.add(r7)); // R7 R8
+			assertTrue(testMeld.isRun());
+			assertFalse(testMeld.isSet());
+			assertTrue(testMeld.add(r6)); // R6 R7 R8
+			assertTrue(testMeld.isRun());
+			assertFalse(testMeld.isSet());
+			assertEquals(r6, testMeld.removeHead()); // R7 R8
+			assertTrue(testMeld.isRun());
+			assertFalse(testMeld.isSet());
+			assertEquals(r7, testMeld.removeHead()); // R8
+			assertTrue(testMeld.isRun());
+			assertTrue(testMeld.isSet());
+		} catch (AbleToAddBothSideException e) {
+			fail();
+		}
 	}
-	
+
 	public void testIsRunAndIsSetWithJoker() {
 		init();
-		assertTrue(testMeld.isRun());
-		assertTrue(testMeld.isSet());
-		assertTrue(testMeld.addHead(joker)); // jk
-		assertTrue(testMeld.isRun());
-		assertTrue(testMeld.isSet());
-		assertTrue(testMeld.addTail(r8)); // jk R8
-		assertTrue(testMeld.isRun());
-		assertTrue(testMeld.isSet());
-		assertTrue(testMeld.addTail(o8)); // jk R8 O8
-		assertFalse(testMeld.isRun());
-		assertTrue(testMeld.isSet());
-		assertEquals(joker, testMeld.removeHead()); // R8 O8
-		assertFalse(testMeld.isRun());
-		assertTrue(testMeld.isSet());
-		assertTrue(testMeld.addTail(joker));// R8 O8 jk
-		assertFalse(testMeld.isRun());
-		assertTrue(testMeld.isSet());
-		assertEquals(r8, testMeld.removeHead()); //O8 jk
-		assertTrue(testMeld.isRun());
-		assertTrue(testMeld.isSet());
-		assertEquals(o8, testMeld.removeHead()); //jk
-		assertTrue(testMeld.isRun());
-		assertTrue(testMeld.isSet());
-		assertTrue(testMeld.addHead(r6));//R6 jk
-		assertTrue(testMeld.isRun());
-		assertTrue(testMeld.isSet());
-		assertTrue(testMeld.addHead(r5));//R5 R6 jk
-		assertTrue(testMeld.isRun());
-		assertFalse(testMeld.isSet());
-		assertEquals(r5, testMeld.removeHead()); //R6 jk
-		assertTrue(testMeld.isRun());
-		assertTrue(testMeld.isSet());
-		assertTrue(testMeld.addHead(r7));// R6 jk R7
-		assertTrue(testMeld.isRun());
-		assertFalse(testMeld.isSet());
-		assertEquals(r6, testMeld.removeHead()); //jk R7
-		assertTrue(testMeld.isRun());
-		assertTrue(testMeld.isSet());
+		try {
+			assertTrue(testMeld.isRun());
+			assertTrue(testMeld.isSet());
+			assertTrue(testMeld.addHead(joker)); // jk
+			assertTrue(testMeld.isRun());
+			assertTrue(testMeld.isSet());
+			assertTrue(testMeld.add(r8)); // jk R8
+			assertTrue(testMeld.isRun());
+			assertTrue(testMeld.isSet());
+			assertTrue(testMeld.add(o8)); // jk R8 O8
+			assertFalse(testMeld.isRun());
+			assertTrue(testMeld.isSet());
+			assertEquals(joker, testMeld.removeHead()); // R8 O8
+			assertFalse(testMeld.isRun());
+			assertTrue(testMeld.isSet());
+			assertTrue(testMeld.addTail(joker));// R8 O8 jk
+			assertFalse(testMeld.isRun());
+			assertTrue(testMeld.isSet());
+			assertEquals(r8, testMeld.removeHead()); // O8 jk
+			assertTrue(testMeld.isRun());
+			assertTrue(testMeld.isSet());
+			assertEquals(o8, testMeld.removeHead()); // jk
+			assertTrue(testMeld.isRun());
+			assertTrue(testMeld.isSet());
+			assertTrue(testMeld.add(r6));// R6 jk
+			assertTrue(testMeld.isRun());
+			assertTrue(testMeld.isSet());
+			assertTrue(testMeld.add(r5));// R5 R6 jk
+			assertTrue(testMeld.isRun());
+			assertFalse(testMeld.isSet());
+			assertEquals(r5, testMeld.removeHead()); // R6 jk
+			assertTrue(testMeld.isRun());
+			assertTrue(testMeld.isSet());
+			assertTrue(testMeld.add(r7));// R6 jk R7
+			assertTrue(testMeld.isRun());
+			assertFalse(testMeld.isSet());
+			assertEquals(r6, testMeld.removeHead()); // jk R7
+			assertTrue(testMeld.isRun());
+			assertTrue(testMeld.isSet());
+		} catch (AbleToAddBothSideException e) {
+			fail();
+		}
 	}
 }
