dbb4071f51ecea7758560decdf59d8a44c392943
Yuanming Xiao <china.hunterxiao@gmail.com>
2018-10-14 15:51:27 -0400

    *HandTest.java
    Change getHand() to getTile()

--- src/test/java/rrrummy/HandTest.java
+++ src/test/java/rrrummy/HandTest.java
@@ -28,22 +28,22 @@ public class HandTest extends TestCase{
 			assertEquals(7, testHand.size());
 			testHand.add(new Tile("G10"));
 			assertEquals(8, testHand.size());
-			assertEquals(Tile.Color.RED, testHand.getHand().get(0).getColor());
-			assertEquals(5, testHand.getHand().get(0).getNumber());
-			assertEquals(Tile.Color.BLUE, testHand.getHand().get(1).getColor());
-			assertEquals(3, testHand.getHand().get(1).getNumber());
-			assertEquals(Tile.Color.ORANGE, testHand.getHand().get(2).getColor());
-			assertEquals(6, testHand.getHand().get(2).getNumber());
-			assertEquals(Tile.Color.JOKER, testHand.getHand().get(3).getColor());
-			assertEquals(0, testHand.getHand().get(3).getNumber());
-			assertEquals(Tile.Color.BLUE, testHand.getHand().get(4).getColor());
-			assertEquals(7, testHand.getHand().get(4).getNumber());
-			assertEquals(Tile.Color.RED, testHand.getHand().get(5).getColor());
-			assertEquals(11, testHand.getHand().get(5).getNumber());
-			assertEquals(Tile.Color.GREEN, testHand.getHand().get(6).getColor());
-			assertEquals(1, testHand.getHand().get(6).getNumber());
-			assertEquals(Tile.Color.GREEN, testHand.getHand().get(7).getColor());
-			assertEquals(10, testHand.getHand().get(7).getNumber());
+			assertEquals(Tile.Color.RED, testHand.getTile(0).getColor());
+			assertEquals(5, testHand.getTile(0).getNumber());
+			assertEquals(Tile.Color.BLUE, testHand.getTile(1).getColor());
+			assertEquals(3, testHand.getTile(1).getNumber());
+			assertEquals(Tile.Color.ORANGE, testHand.getTile(2).getColor());
+			assertEquals(6, testHand.getTile(2).getNumber());
+			assertEquals(Tile.Color.JOKER, testHand.getTile(3).getColor());
+			assertEquals(0, testHand.getTile(3).getNumber());
+			assertEquals(Tile.Color.BLUE, testHand.getTile(4).getColor());
+			assertEquals(7, testHand.getTile(4).getNumber());
+			assertEquals(Tile.Color.RED, testHand.getTile(5).getColor());
+			assertEquals(11, testHand.getTile(5).getNumber());
+			assertEquals(Tile.Color.GREEN, testHand.getTile(6).getColor());
+			assertEquals(1, testHand.getTile(6).getNumber());
+			assertEquals(Tile.Color.GREEN, testHand.getTile(7).getColor());
+			assertEquals(10, testHand.getTile(7).getNumber());
 			
 			//remove
 			Tile t = testHand.remove();
@@ -73,22 +73,22 @@ public class HandTest extends TestCase{
 			//R5 B3 O6 J B7 R11 G1 G10
 			testHand.sort();
 			//B3 B7 R5 R11 G1 G10 O6 J
-			assertEquals(Tile.Color.BLUE, testHand.getHand().get(0).getColor());
-			assertEquals(Tile.Color.BLUE, testHand.getHand().get(1).getColor());
-			assertEquals(Tile.Color.RED, testHand.getHand().get(2).getColor());
-			assertEquals(Tile.Color.RED, testHand.getHand().get(3).getColor());
-			assertEquals(Tile.Color.GREEN, testHand.getHand().get(4).getColor());
-			assertEquals(Tile.Color.GREEN, testHand.getHand().get(5).getColor());
-			assertEquals(Tile.Color.ORANGE, testHand.getHand().get(6).getColor());
-			assertEquals(Tile.Color.JOKER, testHand.getHand().get(7).getColor());
-			assertEquals(3, testHand.getHand().get(0).getNumber());
-			assertEquals(7, testHand.getHand().get(1).getNumber());
-			assertEquals(5, testHand.getHand().get(2).getNumber());
-			assertEquals(11, testHand.getHand().get(3).getNumber());
-			assertEquals(1, testHand.getHand().get(4).getNumber());
-			assertEquals(10, testHand.getHand().get(5).getNumber());
-			assertEquals(6, testHand.getHand().get(6).getNumber());
-			assertEquals(0, testHand.getHand().get(7).getNumber());
+			assertEquals(Tile.Color.BLUE, testHand.getTile(0).getColor());
+			assertEquals(Tile.Color.BLUE, testHand.getTile(1).getColor());
+			assertEquals(Tile.Color.RED, testHand.getTile(2).getColor());
+			assertEquals(Tile.Color.RED, testHand.getTile(3).getColor());
+			assertEquals(Tile.Color.GREEN, testHand.getTile(4).getColor());
+			assertEquals(Tile.Color.GREEN, testHand.getTile(5).getColor());
+			assertEquals(Tile.Color.ORANGE, testHand.getTile(6).getColor());
+			assertEquals(Tile.Color.JOKER, testHand.getTile(7).getColor());
+			assertEquals(3, testHand.getTile(0).getNumber());
+			assertEquals(7, testHand.getTile(1).getNumber());
+			assertEquals(7, testHand.getTile(2).getNumber());
+			assertEquals(7, testHand.getTile(3).getNumber());
+			assertEquals(7, testHand.getTile(4).getNumber());
+			assertEquals(7, testHand.getTile(5).getNumber());
+			assertEquals(7, testHand.getTile(6).getNumber());
+			assertEquals(7, testHand.getTile(7).getNumber());			
 		}catch(InvalidTileException e) {
 			fail(e.getErrMsg());
 		}
