43d8403815f7f1137a6ded41db6c2133400cb434
Yuanming Xiao <china.hunterxiao@gmail.com>
2018-10-16 14:40:56 -0400

    + GameData & GameDataTest class
    added GameData.java & test case GameDataTest.java

---
+++ src/main/java/rrrummy_AI/GameData.java
@@ -0,0 +1,25 @@
+package rrrummy_AI;
+
+import java.util.ArrayList;
+
+public class GameData implements Subject{
+
+	@Override
+	public void registerObserver(Observer o) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeObserver(Observer o) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void notifyObservers() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
---
+++ src/test/java/rrrummy_AI/GameDataTest.java
@@ -0,0 +1,19 @@
+package rrrummy_AI;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GameDataTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
