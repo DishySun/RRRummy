22b322dc889b68400fea9372e70ed4cfab5519ed
Yuanming Xiao <china.hunterxiao@gmail.com>
2018-10-16 16:30:06 -0400

    * test case for GameData
    modified GameDataTest.java

--- src/test/java/rrrummy_AI/GameDataTest.java
+++ src/test/java/rrrummy_AI/GameDataTest.java
@@ -2,18 +2,31 @@ package rrrummy_AI;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.Test;
 
 public class GameDataTest {
-
+	private Observer observers;
+	private GameData testGameData;
 	@Before
 	public void setUp() throws Exception {
+		testGameData = new GameData();
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_registerObserver() {
+		Observer observer = null;
+		testGameData.registerObserver(observer);
+		assertEquals(1,testGameData.getObserversSize());
 	}
 
+	@Test
+	public void test_removeObserver() {
+		Observer observer = null;
+		testGameData.registerObserver(observer);
+		testGameData.removeObserver(observer);
+		assertEquals(0,testGameData.getObserversSize());
+	}
 }
