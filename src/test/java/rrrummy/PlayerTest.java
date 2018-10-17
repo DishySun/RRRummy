4e5d2b1dd77a41aa725f578318723bd8a81f7b77
De Tao <td950614@gmail.com>
2018-10-17 16:38:39 -0400

    *PlayerTest.java
    add more test cases in PlayerTest.java

--- src/test/java/rrrummy/PlayerTest.java
+++ src/test/java/rrrummy/PlayerTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 public class PlayerTest {
 	private Player testPlayer;	
 	private Tile tile1;
+	private Tile tile2;
 	@Before
 	public void setUp() throws Exception {
 		testPlayer = new Player("Hunter");
@@ -21,15 +22,30 @@ public class PlayerTest {
 	@Test
 	public void test_play() {
 		testPlayer.draw(tile1);
+		assertEquals(1, testPlayer.handSize());
 		testPlayer.play(0);
 		assertEquals(0, testPlayer.handSize());
 	}
+	
+	public void test_play2() {
+		testPlayer.draw(tile1);
+		assertEquals(1, testPlayer.handSize());
+		testPlayer.draw(tile2);
+		assertEquals(2, testPlayer.handSize());
+		testPlayer.play(1);
+		assertEquals(1, testPlayer.handSize());
+	}
+	
+	
 
 	@Test
 	public void test_draw() {
 		testPlayer.draw(tile1);
 		assertEquals(1, testPlayer.handSize());
 		assertEquals(tile1, testPlayer.getHand(0));
+		testPlayer.draw(tile2);
+		assertEquals(2, testPlayer.handSize());
+		assertEquals(tile2, testPlayer.getHand(1));
 	}
 	
 	@Test
