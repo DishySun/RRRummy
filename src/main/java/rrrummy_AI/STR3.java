4f6595bdc6319ff0d6e5e24518dd3f16020d66e0
Yuanming Xiao <china.hunterxiao@gmail.com>
2018-10-25 07:16:15 -0400

    * STR3Test.java & STR3.java
    added test case for STR3, edited STR3 (not done)

--- src/main/java/rrrummy_AI/STR3.java
+++ src/main/java/rrrummy_AI/STR3.java
@@ -4,38 +4,104 @@ import java.util.ArrayList;
 
 import rrrummy.Player;
 import rrrummy.Table;
+import rrrummy.Tile;
 
 public class STR3  implements Observer, AIstrategy{
 	private Table table;
 	private ArrayList<Player> players;
+	private ArrayList<Tile> tile4Run;
+	private ArrayList<Tile> tile4Group;
+	private ArrayList<Tile> tile4All;
+	private ArrayList<Tile> tile4MeldT;
+	private AI AI;
 	public STR3(Subject data) {
 		data.registerObserver(this);
 		players = new ArrayList<Player>();
+		AI = (AI)players.get(0);
 	}
 	
 	/**
 	 * play as soon as possible
 	 */
 	@Override
-	public void playInitial(Table table) {
+	public void playInitial() {
 		// TODO Auto-generated method stub
-		
+		tile4Run = new ArrayList<Tile>();
+		tile4Group = new ArrayList<Tile>();
+		tile4Run = AI.findInitRun();
+		tile4Group = AI.findInitGroup();
+		if(tile4Run.size() > tile4Group.size()) {
+			for(Tile t: tile4Run) {
+				AI.play(AI.play(t));
+			}
+		} else {
+			for(Tile t: tile4Group) {
+				AI.play(AI.play(t));
+			}
+		}
 	}
 
 	/**
-	 * if no other players hand.size< this.hand.size - 3, only play tiles that form meld on table
-	 * else, play all tiles it can
+	 * if it can play all tiles(possibly using what's on table) it does;
+	 * else:
+	 * 		if no other players hand.size< this.hand.size - 3, only play tiles that form meld on table
+	 * 		else, play all tiles it can
 	 */
 	@Override
-	public void playRest(Table table, ArrayList<Player> players) {
+	public void playRest() {
 		// TODO Auto-generated method stub
+		tile4Run = new ArrayList<Tile>();
+		tile4Group = new ArrayList<Tile>();
+		tile4All = new ArrayList<Tile>();
+		tile4Group = new ArrayList<Tile>();
+		boolean handsless3 = false;
+		for(Player p : players) {
+			if(p.handSize() < AI.handSize()-3) {
+				handsless3 = true;
+				break;
+			}
+		}
 		
+		tile4All = AI.findBest2Play(table);
+		if(tile4All.size() == AI.handSize()) {
+			for(Tile t: tile4All) {
+				AI.play(AI.play(t));/////
+			}
+		} else {
+			if(handsless3) {
+				tile4MeldT = AI.findMeldsOnTable(table);
+				for(Tile t: tile4All) {
+					AI.play(AI.play(t));////////
+				}
+			} else {
+				tile4Run = AI.findRun();
+				tile4Group = AI.findGroup();
+				if(tile4Run.size() > tile4Group.size()) { //first run
+					for(Tile t: tile4Run) {
+						AI.play(AI.play(t));
+					}
+					tile4Group = AI.findGroup();
+					for(Tile t: tile4Group) {
+						AI.play(AI.play(t));
+					}
+				} else {		//first group
+					for(Tile t: tile4Group) {
+						AI.play(AI.play(t));
+					}
+					tile4Run = AI.findRun();
+					for(Tile t: tile4Run) {
+						AI.play(AI.play(t));
+					}
+				}
+			}
+		}
 	}
 
 	@Override
 	public void update(Table melds, ArrayList<Player> players) {
 		// TODO Auto-generated method stub
-		
+		this.table = melds;
+		this.players = players;
 	}
 
 }
---
+++ src/test/java/rrrummy_AI/STR3Test.java
@@ -0,0 +1,115 @@
+package rrrummy_AI;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import rrrummy.InvalidTileException;
+import rrrummy.Player;
+import rrrummy.Table;
+import rrrummy.Tile;
+
+public class STR3Test {
+	private Tile atile1,atile2,atile3,atile4,atile5,atile6,atile7,atile8,atile9,atile10,atile11,atile12,atile13;
+	private Tile aJoker;
+	private Tile bJoker;
+	private AI ai;
+	private AIstrategy aISty;
+	private GameData data;
+	private Table table;
+	private ArrayList<Player> players;
+	@Before
+	public void setUp() throws Exception {
+		table = new Table();
+		ai = new AI("Hunter");
+		data = new GameData();
+		aISty = new STR3(data);
+		ai.setSTY(aISty);
+		players = new ArrayList<Player>();
+	}
+
+	@Test
+	public void test_playInitial1() {
+		try {
+			atile1 = new Tile("R1");
+			atile2 = new Tile("R2");
+			atile3 = new Tile("R3");
+			atile4 = new Tile("R4");
+			atile5 = new Tile("R5");
+			atile6 = new Tile("B6");
+			atile7 = new Tile("O7");
+			atile8 = new Tile("R8");
+			atile9 = new Tile("G9");
+			atile10 = new Tile("R10");
+			atile11 = new Tile("R11");
+			atile12 = new Tile("R12");
+			atile13 = new Tile("R13");
+			aJoker = new Tile("J");
+		}catch(InvalidTileException e) {
+			fail();
+		}
+		ai.draw(atile1);
+		ai.draw(atile2);
+		ai.draw(atile3);
+		ai.draw(atile4);
+		ai.draw(atile5);
+		ai.draw(atile6);
+		ai.draw(atile7);
+		ai.draw(atile8);
+		ai.draw(atile9);
+		ai.draw(atile10);
+		ai.draw(atile11);
+		ai.draw(atile12);
+		ai.draw(atile13);
+		ai.draw(aJoker);
+		
+		players.add(ai);
+		data.setValue(table, players);
+		ai.getSTY().playInitial();
+		assertEquals(8, ai.handSize());
+		ai.getSTY().playRest();
+	}
+
+	@Test
+	public void test_playRest1() {
+		try {
+			atile1 = new Tile("R1");
+			atile2 = new Tile("R2");
+			atile3 = new Tile("R3");
+			atile4 = new Tile("R4");
+			atile5 = new Tile("R5");
+			atile6 = new Tile("B6");
+			atile7 = new Tile("O7");
+			atile8 = new Tile("R8");
+			atile9 = new Tile("G9");
+			atile10 = new Tile("R10");
+			atile11 = new Tile("R11");
+			atile12 = new Tile("R12");
+			atile13 = new Tile("R13");
+			aJoker = new Tile("J");
+		}catch(InvalidTileException e) {
+			fail();
+		}
+		ai.draw(atile1);
+		ai.draw(atile2);
+		ai.draw(atile3);
+		ai.draw(atile4);
+		ai.draw(atile5);
+		ai.draw(atile6);
+		ai.draw(atile7);
+		ai.draw(atile8);
+		ai.draw(atile9);
+		ai.draw(atile10);
+		ai.draw(atile11);
+		ai.draw(atile12);
+		ai.draw(atile13);
+		ai.draw(aJoker);
+		
+		players.add(ai);
+		data.setValue(table, players);
+		ai.getSTY().playRest();
+	}
+}
