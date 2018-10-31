aaf5fe49a01794d0709fb9b16716e3d104151abe
選ばれた観測者 <dishysun@hotmail.com>
2018-10-31 01:31:56 -0400

    add note to available all commands

--- src/main/java/command/Command.java
+++ src/main/java/command/Command.java
@@ -1,5 +1,22 @@
 package command;
 
+/*Valid command:
+ * Play:
+ * 		- Play int(hand index) 
+ * 		- Play int(hand index) to int(meld index)
+ * 		- Play int(hand index) to int(meld index) head/tail
+ * 		- Play ArrayList<Integer>(hand indexs)
+ * Move:
+ * 		- Move int(meld) head/tail to int(meld)
+ * 		- Move int head/tail to int head/tail
+ * Cut:
+ * 		- Cut int at int
+ * Replace:
+ * 		- Replace int to int int
+ * End:
+ * 		- End
+ * */
+
 public interface Command {
 	public void excute();
 	public String toString();
