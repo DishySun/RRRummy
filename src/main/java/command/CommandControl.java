e86e8129f3da1138b725d61bf55550cbd50570d1
選ばれた観測者 <dishysun@hotmail.com>
2018-11-01 11:22:20 -0400

    + Play command feature
     * Available Play Command: (latter cases doesn't matter)
     - String("play") int(hand index) 
     - String("play") int(hand index) String("to") int(meld index)
     - String("play") int(hand index) String("to") int(meld index) head/tail
     - String("play") ArrayList<Integer>(hand indexes)

--- src/main/java/command/CommandControl.java
+++ src/main/java/command/CommandControl.java
@@ -1,22 +1,93 @@
 package command;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
-import players.Player;
+import game.View;
 import rrrummy.Game;
 
 public class CommandControl {
 	private ArrayList<Command> commandHistory;
+	private View view;
 	
-	public CommandControl() {
+	
+	public CommandControl(View v) {
 		commandHistory = new ArrayList<Command>();
+		view = v;
 	}
 	
-	public void newCommand(Game game, String command, Player p) {
-		if (command.compareToIgnoreCase("end") == 0) {
-			Command c = new EndTurnCommand(game);
-			commandHistory.add(c);
-			c.excute();
+	
+	public int newCommand(Game game, String command) {
+		// return -1 if command is not valid, 0 for excute fail, 1 for success
+		command = command.toLowerCase();
+		ArrayList<String> commandList = new ArrayList<String>(Arrays.asList(command.split("\\s+")));
+		commandList.remove("");
+		Command c = null;
+		switch (commandList.remove(0)) {
+		case "play":
+			ArrayList<Integer> intList = new ArrayList<Integer>();
+			String temp = "";
+			int meldIndex = -1;
+			try{
+				while(true){
+					temp = commandList.remove(0);
+					intList.add(Integer.valueOf(temp));
+				}
+			}catch(java.lang.NumberFormatException e){
+			    //to
+				if (!temp.equalsIgnoreCase("to")) return -1;
+				//TODO:adding a List of indexes to a specific meld feature will be implemented 
+				if (intList.size() != 1) return -1;
+				try {
+					temp = commandList.remove(0);
+					meldIndex = Integer.valueOf(temp);
+				}catch(Exception ee) {
+					return -1;
+				}
+				try {
+					temp = commandList.remove(0);
+				}catch (java.lang.IndexOutOfBoundsException ee) {
+					//play int to int
+					c = new PlayCommand(intList.get(0), meldIndex, game, view);
+					break;
+				}
+				switch (temp) {
+				// play int to int head/tail
+				case "head":
+					c = new PlayCommand(intList.get(0), meldIndex, 0, game);
+					break;
+				case "tail":
+					c = new PlayCommand(intList.get(0), meldIndex, 1, game);
+					break;
+				default: return -1;
+				}
+		    }catch(java.lang.IndexOutOfBoundsException e){
+		    	//no "to"
+		    	if (intList.size() == 0) return -1;
+		    	if (intList.size() == 1) {
+		    		//play int
+		    		c = new PlayCommand(intList.get(0), game);
+		    	}else {
+		    		//play arraylist
+		    		c = new PlayCommand(intList, game);
+		    	}
+		    }
+			//return -1;
+		case "move":break;
+		case "cut":break;
+		case "replace":break;
+		case "end": 
+			c = new EndTurnCommand(game);
+			break;
+		default: return -1;
 		}
+		
+		if (command.compareToIgnoreCase("end") == 0) {
+			c = new EndTurnCommand(game);
+		}
+		commandHistory.add(c);
+		if(c.excute()) return 1;
+		return 0;
+		
 	}
 }
--- src/main/java/command/PlayCommand.java
+++ src/main/java/command/PlayCommand.java
@@ -1,7 +1,10 @@
 package command;
 
-import players.Player;
-import rrrummy.Tile;
+import java.util.ArrayList;
+
+import game.View;
+import rrrummy.AbleToAddBothSideException;
+import rrrummy.Game;
 
 public class PlayCommand implements Command{
 	/* Correct Form:
@@ -11,17 +14,69 @@ public class PlayCommand implements Command{
 	 * 3.	Play		2				to			4							==> playerPlays(Tile t ,int toMeld)
 	 * 4. 	Play		2				to 			4 				head
 	 * */
-	private Player player;
+	private Game game;
+	private View view;
+	private int handIndex;
+	private int meldIndex;
+	private int headOrTail; // -1 for null, 0 for head, else for tail
+	private ArrayList<Integer> arr;
 	
-	public PlayCommand(String commandString, Player player) {
-		this.player = player;
+	public PlayCommand(int h, Game g) {
+		//play int
+		handIndex = h;
+		meldIndex = -1;
+		headOrTail = -1;
+		game = g;
+		arr = null;
+	}
+	
+	public PlayCommand(int h, int m, Game g , View v) {
+		//play int to int
+		handIndex = h;
+		meldIndex = m;
+		game = g;
+		headOrTail  = -1;
+		arr = null;
+		view = v;
+	}
+	
+	public PlayCommand(int h, int m, int b, Game g) {
+		//play int to int HorT
+		handIndex = h;
+		meldIndex = m;
+		headOrTail = b;
+		game = g;
+		arr = null;
+	}
+	
+	public PlayCommand(ArrayList<Integer> a, Game g) {
+		//play arraylist
+		handIndex = -1;
+		meldIndex = -1;
+		headOrTail = -1;
+		arr = a;
+		game = g;
 	}
 	
 	@Override
-	public void excute() {
-		// TODO Auto-generated method stub
-		
+	public boolean excute(){
+		if (meldIndex == -1) {
+			//game.playerPlays(int) /game.playerPlays(ArrayList)
+			if (arr == null) return game.playerPlays(handIndex);
+			else return game.playerPlays(arr);
+		}else {
+			//game.playerPlays(int,int) / game.playerPlays(int, int, boolean)
+			switch (headOrTail) {
+			case -1:	try{
+							return game.playerPlays(handIndex, meldIndex);
+						}catch(AbleToAddBothSideException e) {
+							String str = view.getHeadOrTail("play");
+							if(str.equalsIgnoreCase("head")) return game.playerPlays(handIndex, meldIndex, true);
+							else return game.playerPlays(handIndex, meldIndex, false);
+						}
+			case 0:		return game.playerPlays(handIndex, meldIndex, true);
+			default:	return game.playerPlays(handIndex, meldIndex, false);
+			}
+		}
 	}
-	
-
 }
